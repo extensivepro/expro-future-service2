7a2810dc16d1466489abfb1e7e3be8a35ada9edd
Guan Bo <guanbo2002@gmail.com>
2014-10-13 17:51:41 +0800

    Add: member add

--- Dashboard/src/js/dashboard/controllers/members-ctrl.js
+++ Dashboard/src/js/dashboard/controllers/members-ctrl.js
@@ -1,8 +1,45 @@
 /**
  * Members Controller
  */
-app.controller('MembersCtrl', function MembersCtrl($scope, Member, $controller) {
+app.controller('MembersCtrl', function MembersCtrl($scope, Member, $controller, $modal) {
   $controller('ListCtrl', {$scope: $scope})
   $scope.resource = Member
   $scope.search.orFields = ['name', 'phone']
+  
+  $scope.create = function () {
+    var modalInstance = $modal.open({
+      templateUrl: 'partials/member-add.html',
+      controller: CreateMemberModalInstanceCtrl,
+      size: 'lg'
+    });
+
+    modalInstance.result.then(function (member) {
+    }, function () {
+      console.info('Modal dismissed at: ' + new Date());
+    });
+  }
+  
 })
+
+var CreateMemberModalInstanceCtrl = function ($scope, $modalInstance, $rootScope, Member) {
+
+  $scope.entity = {}
+  
+  $scope.alerts = []
+	
+  $scope.tryCreate = function () {
+    Member.login($scope.entity, function (member) {
+      $modalInstance.close(member);
+    }, function (res) {
+      $scope.alerts.push({type: 'danger', msg: '创建会员失败'})
+    })
+  }
+  
+  $scope.cancel = function () {
+    $modalInstance.dismiss()
+  }
+  
+  $scope.blurCb = function (evt) {
+    $scope.entity.code = $scope.entity.phone
+  }
+}
