61ef13f50c7b106f7e7195ce219c9cbe049d85e9
Guan Bo <guanbo2002@gmail.com>
2014-10-13 14:53:02 +0800

    Update: bower expro future service2

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var rename = require('gulp-rename');
 var loopbackAngular = require('gulp-loopback-sdk-angular');
 
-gulp.task('default', function () {
+gulp.task('generate-angularjs-sdk',function () {
   return gulp.src('./server/server.js')
     .pipe(loopbackAngular({apiUrl:'http://fankahui.com:3000/api'}))
-    .pipe(rename('ef-services.js'))
-    .pipe(gulp.dest('./Dashboard/src/js/dashboard'));
-});
+    .pipe(rename('ef-services2.js'))
+    .pipe(gulp.dest('../bower-ef-services2'));
+})
+
+gulp.task('default', ['generate-angularjs-sdk']);
