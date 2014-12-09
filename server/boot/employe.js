a277d7cfb230c08023bdaf2c63ba5e5bcc4a0535
Guan Bo <guanbo2002@gmail.com>
2014-12-09 13:42:24 +0800

    Add: ACL
    close #2

--- server/boot/employe.js
+++ server/boot/employe.js
@@ -1,29 +1,45 @@
-var authorized_paths = ['/members', '/bills', '/deals', '/employes']
+var authorized_paths = ['/members', '/bills', '/deals', '/employes', '/points']
+
 module.exports = function(app) {
-  app.use('/api',function(req, res, next) {
+	
+  var restApiRoot = app.get('restApiRoot')
+
+	app.use(restApiRoot, function (req, res, next) {
+
+		if (req.method === 'OPTIONS') return next()
+
 		if(req.accessToken) {
-      app.models.User.findOne({where:{id:req.accessToken.userId}, include:{employe:'merchant'}}, function (err, user) {
+      app.models.User.findOne({
+				where:{id:req.accessToken.userId},
+				include:{employe:'merchant'}
+			}, function (err, user) {
         req.employe = user.employe()
-				if(authorized_paths.indexOf(req.path) !== -1) {
-					if(req.employe) {
-						if(req.method === 'GET') {
-							req.query.filter = req.query.filter || '{}'
-							var filter = JSON.parse(req.query.filter)
-							filter.where = {merchantID: req.employe.merchantID}
-							req.query.filter = JSON.stringify(filter)
-						}
-						next()
-					} else {
-			      var error = new Error('unauthorized, unkonwn employe.')
-			      error.status = 401
-						next(error)
-					}
-				} else {
-					next()
-				}
+				next(err)
 			})
 		} else {
 			next()
 		}
-  })
+		
+	})
+	
+	app.use(restApiRoot, function (req, res, next) {
+		
+		if(authorized_paths.indexOf(req.path) !== -1 && req.method === 'GET') {
+			if(req.employe) {
+				req.query.filter = req.query.filter || '{}'
+				var filter = JSON.parse(req.query.filter)
+				filter.where = {merchantID: req.employe.merchantID}
+				req.query.filter = JSON.stringify(filter)
+				next()
+			} else {
+	      var error = new Error('unauthorized, unkonwn employe.')
+	      error.status = 401
+				next(error)
+			}
+		} else {
+			next()
+		}
+		
+	})
+	
 }
