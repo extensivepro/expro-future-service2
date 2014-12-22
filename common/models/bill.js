f6cb6f34fb2cc929bc7f951d33d132b57bb4baa6
Guan Bo <guanbo2002@gmail.com>
2014-12-22 21:45:43 +0800

    Update: bill validation

--- common/models/bill.js
+++ common/models/bill.js
@@ -2,38 +2,62 @@ module.exports = function(Bill) {
 	
   Bill.beforeCreate = function (next, bill) {
     if(bill.amount <= 0) {
-      var error = new Error('Bad bill, bill can not be 0')
+      var error = new Error('Bad bill, bill amount can not be 0')
       error.status = 400
       return next(error)
     }
     var now = Math.floor(Date.now()/1000)
     bill.createdAt = bill.createdAt || now
     if(!bill.discountAmount) bill.discountAmount = 0
-    next()
+
+    checkBalance(bill.memberSettlement, next)
   }
   
-  var settle = function (account, amount, next) {
+  var checkBalance = function (settlement, next) {
+    
+    if(!(settlement && settlement.payerAccount)) return next()
+    
+    var account = settlement.payerAccount
+    
     Bill.app.models.Account.findById(account.id, function (err, account) {
       if(err) return next(err)
-      account.balance += amount;
-      account.save()
-      Bill.app.models.Member.update({"account.id": account.id}, {account: account}, next)
+      
+      if(account.balance < settlement.amount) {
+        err = new Error('Bad bill, not sufficient funds of member balance')
+        err.status = 400
+        return next(err)
+      }
+      
+      next()
     })
+  }
+  
+  var settleWithMember = function (settlement, next) {
     
+    if(!settlement) return next()
+      
+    var amount = settlement.amount    
+    var account = settlement.payerAccount
+    
+    if(account) {
+      amount = 0-amount
+    } else {
+      account = settlement.payeeAccount
+    }
+
+    Bill.app.models.Account.findById(account.id, function (err, account) {
+      if(err) return next(err)
+      account.balance += amount
+      account.save()
+      Bill.app.models.Member.update({"account.id": account.id}, {account: account}, function (err, count) {
+        next(err)
+      })
+    })
+
   }
   
   Bill.afterCreate = function (next) {
-    
-    if(this.memberSettlement) {
-      if(this.memberSettlement.payeeAccount) {
-        settle(this.memberSettlement.payeeAccount, this.memberSettlement.amount, next)
-      } else if(this.memberSettlement.payerAccount) {
-        settle(this.memberSettlement.payerAccount, 0-this.memberSettlement.amount, next)
-      } else {
-        next()
-      }
-    } else {
-      next()
-    }
+    settleWithMember(this.memberSettlement, next)
   }
+
 };
