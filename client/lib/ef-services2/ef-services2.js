882d287bacedb3f0946fd26fc3a64c3a0c7531ee
Guan Bo <guanbo2002@gmail.com>
2014-12-29 10:14:15 +0800

    Update: ef service 2

--- client/lib/ef-services2/ef-services2.js
+++ client/lib/ef-services2/ef-services2.js
@@ -2,7 +2,7 @@
 (function(window, angular, undefined) {'use strict';
 
 var urlBase = "http://fankahui.com:3000/api";
-urlBase = "http://0.0.0.0:3000/api";
+// urlBase = "http://0.0.0.0:3000/api";
 var authHeader = 'authorization';
 
 /**
@@ -8541,10 +8541,12 @@ module.factory(
           include:['merchant', 'shop']
         }
       }, function (employe) {
+        props = Object.keys(employe)
         props.forEach(function (name) {
           self[name] = employe[name]
           save(name, self[name])
         })
+        // $rootScope.$broadcast('CURRENT_EMPLOYE_READY')
       }, function (res) {
         console.log('Find employe error')
       })
@@ -8561,13 +8563,12 @@ module.factory(
   return new CurrentEmploye()
   
   function save(name, value) {
-    var key = '$CurrentEmploye$' + name
-    if (value == null) value = ''
-    localStorage[key] = value
+    var key = '$EFCurrentEmploye$' + name
+    localStorage[key] = value || ''
   }
   
   function load(name) {
-    return localStorage['$CurrentEmploye$' + name] || null
+    return localStorage['$EFCurrentEmploye$' + name] || null
   }
 });
 
@@ -8575,14 +8576,17 @@ module.factory(
   'DealTransaction', 
   function (CurrentEmploye, Deal) {
 
-  var props = ['serialNumber', 'seller', 'merchantID', 'shopID', 'quantity', 'fee', 'items', 'bill']
+  var props = ['serialNumber', 'seller', 'merchantID', 'shopID', 'quantity', 'fee', 'items', 'bill', 'member', 'buyer']
   
   function DealTransaction() {
-
     props.forEach(function (name) {
       this[name] = load(name)
     }, this)
 
+    this.initDealTransaction()
+  }
+  
+  DealTransaction.prototype.initDealTransaction = function () {
     var now = Date.now()
     var employe = CurrentEmploye
     this.merchantID =  this.merchantID || employe.merchantID,
@@ -8598,6 +8602,7 @@ module.factory(
     this.items = this.items || []
     this.bill = this.bill || {
       amount: 0,
+      discountAmount: 0,
       billNumber: now,
       shopID: employe.shopID,
       merchantID: employe.merchantID,
@@ -8613,7 +8618,7 @@ module.factory(
     }
   }
   
-  DealTransaction.prototype.setMember = function (member) {
+  DealTransaction.prototype.setMember = function (member) {    
     if(!member) {
       this.bill.memberSettlement = null
       this.buyer = null
@@ -8638,7 +8643,7 @@ module.factory(
   }
 
   DealTransaction.prototype.registerItem = function (item) {
-    var exited = this.items.some(function (dealItem) {
+    var found = this.items.some(function (dealItem) {
       if(item.id === dealItem.item.id) {
         dealItem.quantity++
         this.quantity++
@@ -8648,66 +8653,114 @@ module.factory(
         return false
       }
     }, this)
-    if(exited) return
-    this.items.push({
-      item: {
-        id: item.id,
-        "name": item.name,
-        price: item.price
-      },
-      dealPrice: item.price,
-      quantity: 1
-    })        
-    this.quantity += 1
-    this.fee += item.price 
+    if(!found) {
+      var dealItem = {
+        id: uuid.v4(),
+        dealPrice: item.price,
+        quantity: 1,
+        item: {
+          "name": item.name,
+          id: item.id,
+          price: item.price
+        }
+      }
+      this.items.push(dealItem)
+      this.quantity += 1
+      this.fee += dealItem.dealPrice
+    }
   }
   
   DealTransaction.prototype.registerItems = function (items) {
     items.forEach(this.registerItem, this)
   }
   
+  DealTransaction.prototype.account = function () {
+    this.bill.amount = this.fee
+    this.bill.discountAmount = this.bill.discountAmount || 0
+    var payableAmount = this.payableAmount()
+    if(this.bill.memberSettlement) {
+      this.bill.memberSettlement.amount = payableAmount > this.bill.memberSettlement.payerAccount.balance ?  this.bill.memberSettlement.payerAccount.balance : payableAmount
+      payableAmount -= this.bill.memberSettlement.amount
+    }
+    this.bill.cashSettlement.amount = payableAmount
+    
+  }
+  
+  DealTransaction.prototype.payableAmount = function () {
+    return this.bill.amount-this.bill.discountAmount
+  }
+  
+  DealTransaction.prototype.change = function () {
+    var change = this.payableAmount()
+    change -= this.bill.memberSettlement && this.bill.memberSettlement.amount || 0
+    change -= this.bill.cashSettlement && this.bill.cashSettlement.amount || 0
+    return 0-change
+  }
+
   DealTransaction.prototype.settle = function (successCb, errorCb) {
     var validateSettlement = function (settlement) {
       return settlement && settlement.amount > 0 ? settlement : null
     }
     
-    this.bill.amount = this.fee
+    this.bill.amount = this.bill.amount || this.fee
+
+    if(this.bill.memberSettlement &&
+      this.bill.memberSettlement.payerAccount &&
+      this.bill.memberSettlement.payerAccount.balance < this.bill.memberSettlement.amount) {
+      return errorCb(null, {type: 'danger', msg: '客户储值账户余额不足'})
+    }
+    
+    var paidAmount = this.bill.cashSettlement && this.bill.cashSettlement.amount || 0
+    paidAmount += this.bill.memberSettlement && this.bill.memberSettlement.amount || 0
+
+    if(paidAmount < this.bill.amount - this.bill.discountAmount) {
+      return errorCb(null, {type: 'danger', msg: '付款金额不足'})
+    }
+
     this.bill.cashSettlement = validateSettlement(this.bill.cashSettlement)
     this.bill.memberSettlement = validateSettlement(this.bill.memberSettlement)
 
-    var paidAmount = 0
-    if(this.bill.cashSettlement) {
-      paidAmount += this.bill.cashSettlement.amount
-    }
-    if(this.bill.memberSettlement) {
-      paidAmount += this.bill.memberSettlement.amount
-    }
-    if(paidAmount < this.bill.amount - this.bill.discountAmount) {
-      return errorCb(null, {type: 'danger', msg: '支付金额不足'})
-    }
     var entity = {}
     props.forEach(function (name) {
       entity[name] = this[name]
     }, this)
-    Deal.create(entity, successCb, function (res) {
-      errorCb(res, {type: 'danger', msg: '创建失败'})
+    
+    var self = this
+    Deal.create(entity, function (value, responseHeader) {
+      self.close()
+      successCb(arguments)
+    }, function (res) {
+      var error = {type: 'danger', msg: '交易不合规'}
+      if(res.status >= 500) {
+        error.msg = '网络服务不可用'
+      }
+      errorCb(res, error)
     })
+  }
+  
+  DealTransaction.prototype.close = function () {
+    
+    props.forEach(function (name) {
+      this[name] = null
+      save(name, null)
+    }, this)
+    
+    this.initDealTransaction()
   }
   
   return new DealTransaction()
 
   function save(name, value) {
-    var key = '$DealTransaction$' + name
-    if (value == null) value = ''
-    localStorage[key] = value
+    var key = '$EFDealTransaction$' + name
+    localStorage[key] = value || ''
   }
   
   function load(name) {
-    return localStorage['$DealTransaction$' + name] || null
+    return localStorage['$EFDealTransaction$' + name] || null
   }
 
-});
-
+})
+  
 })(window, window.angular);
 
 })();
